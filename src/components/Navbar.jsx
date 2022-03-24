8791ecdcfc8e9abb59ef83f5fe9fcb21c654e296
Inna Groysman <33602729+Innush@users.noreply.github.com>
2022-03-24 12:18:47 -0400

    Added files - react components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+
+const Navbar = () => {
+    return (
+        <nav className="w-full flex md:justify-center justify-between items-center">
+            <h1>Welcome</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
